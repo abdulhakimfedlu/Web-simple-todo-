b8e0e56776f79a85a7644d81008eb0aaf3ae55a0
Abdulhakim Fedlu <155722598+abdulhakimfedlu@users.noreply.github.com>
2025-06-16 21:18:26 +0300

    Update update_task.php

--- update_task.php
+++ update_task.php
@@ -5,6 +5,7 @@ include 'db_connect.php';
 $id = $_POST['id'] ?? 0;
 $completed = $_POST['completed'] ?? 0;
 
+
 try {
     $stmt = $pdo->prepare('UPDATE tasks SET completed = ? WHERE id = ?');
     $stmt->execute([$completed, $id]);
